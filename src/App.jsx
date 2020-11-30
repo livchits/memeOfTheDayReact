4a1e7a0bfd5d8e5c2a61f848bc7da7453612c963
Lucas Livchits <livchitsl@gmail.com>
2020-11-30 20:44:53 -0300

    Add Header to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import './App.css';
 
+import Header from './components/Header';
+
 function App() {
-  return (
-    <div />
-  );
+  return <Header />;
 }
 
 export default App;
