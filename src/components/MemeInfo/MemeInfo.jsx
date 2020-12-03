4a1d1b1d804be6a734d2af274f348c011e70ba9c
Lucas Livchits <livchitsl@gmail.com>
2020-12-03 19:16:58 -0300

    Change prop name

--- src/components/MemeInfo/MemeInfo.jsx
+++ src/components/MemeInfo/MemeInfo.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import './MemeInfo.css';
 
-function MemeInfo({ name, url }) {
+function MemeInfo({ name, imageUrl }) {
   return (
     <figure className="image-container">
-      <img alt={name} className="meme" src={url} />
+      <img alt={name} className="meme" src={imageUrl} />
       <figcaption className="meme-text-container">
         <div className="meme-text">{name}</div>
       </figcaption>
