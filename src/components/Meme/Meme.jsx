4562d0593e4b7a9123d8cb6b2689b29ad4ae7231
Lucas Livchits <livchitsl@gmail.com>
2020-12-03 19:16:24 -0300

    Add logic to ser an actual meme and use its data

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -2,24 +2,32 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 
 import processMemesData from '../../memes/processMemesData';
+import MemeInfo from '../MemeInfo/MemeInfo';
 import Title from '../Title/Title';
+import { getMemeOfTheDay } from '../../memes/utils';
 import './Meme.css';
 
 function Meme() {
   const [memesData, setMemesData] = useState([]);
   const [isRandomMeme, setIsRandomMeme] = useState(false);
+  const [actualMeme, setActualMeme] = useState(null);
 
   useEffect(() => {
     const getMemesData = async () => {
       const memes = await processMemesData();
       setMemesData(memes);
+      const { name, url } = getMemeOfTheDay(memes);
+      setActualMeme({ name, url });
     };
     getMemesData();
   }, []);
 
+  const { name, url } = actualMeme;
+
   return (
     <main className="container">
       <Title isRandomMeme={isRandomMeme} />
+      <MemeInfo imageUrl={url} name={name} />
     </main>
   );
 }
