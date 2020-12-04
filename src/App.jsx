f667498103b32cd827a68a139094f866ff523995
Lucas Livchits <livchitsl@gmail.com>
2020-12-04 19:51:53 -0300

    Add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/Header/Header';
 import Meme from './components/Meme/Meme';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Meme />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer>
+      Created by <a href="https://github.com/livchits">Lucas Livchits</a> 🚀 -
+      2020
+    </footer>
+  );
+}
+
+export default Footer;
